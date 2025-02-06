4f36ae4fb3ded6faa592b3642227a4a0042e75b4
Ilyas Bennajeh <ibennaje@c3r6p10.1337.ma>
2025-02-06 04:51:04 +0100

    mandelbort base code

--- fractol.c
+++ fractol.c
@@ -6,64 +6,62 @@
 /*   By: ibennaje <ibennaje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 23:07:00 by ibennaje          #+#    #+#             */
-/*   Updated: 2025/02/05 17:24:13 by ibennaje         ###   ########.fr       */
+/*   Updated: 2025/02/06 04:48:20 by ibennaje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <stdio.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-int	main(void)
+typedef struct s_data
 {
-	void	*mlx;
-	void	*mlx_win;
-	double x , y;
-	x = y = 0;
+	void *img;
+	char *addr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+} t_data;
+
+int main(void)
+{
+	void *mlx;
+	void *mlx_win;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 900, 900, "Hello world!");
-	while (x < 900)
+	double MinRe = -2.0;
+	int ImageHeight = 900;
+	int ImageWidth = 900;
+	double MaxRe = 0.47;
+	double MinIm = -1.2;
+	double MaxIm = 1.12;
+	double Re_factor = (MaxRe - MinRe) / (ImageWidth - 1);
+	double Im_factor = (MaxIm - MinIm) / (ImageHeight - 1);
+	unsigned int MaxIterations = 100;
+
+	for (int y = 0; y < ImageHeight; ++y)
 	{
-		while (y < 900)
+		double c_im = MaxIm - y * Im_factor;
+		for (int x = 0; x < ImageWidth; ++x)
 		{
-				double c_real = -2;
-				double c_img = 1;
-				double z_real = 0;
-				double z_img = 0;
-				double count = 0;
-				double max_count = 1000;
-				while ((z_real * z_real) + (z_img * z_img) <= 2 * 2 && (count < max_count))
+			double c_re = MinRe + x * Re_factor;
+
+			double Z_re = c_re, Z_im = c_im;
+			int isInside = 1;
+			for (unsigned int n = 0; n < MaxIterations; ++n)
+			{
+				double Z_re2 = Z_re * Z_re, Z_im2 = Z_im * Z_im;
+				if (Z_re2 + Z_im2 > 4)
 				{
-					double next_zreal = (z_real * z_real) + c_real;
-					double next_zimg = (z_img * z_img) + c_img;
-					z_real = next_zreal;
-					z_img = next_zimg;
-					count++;
+					isInside = 0;
+					break;
 				}
-				if (count == max_count)
-				{
-					mlx_pixel_put(mlx, mlx_win, x, y, 0x000000);
-					printf("here\n");		
-				}
-				else 
-				{
-					mlx_pixel_put(mlx, mlx_win, x, y, 0xFF0000);
-					printf("no here\n");
-				}	
-				y++;
-				
+				Z_im = 2 * Z_re * Z_im + c_im;
+				Z_re = Z_re2 - Z_im2 + c_re;
+			}
+			if (isInside)
+				mlx_pixel_put(mlx, mlx_win, x, y, 0xFFFFFF);
 		}
-		x++;
 	}
-	
-	//mlx_pixel_put(mlx, mlx_win, 450, 450, 0xFFFFFF);
 	mlx_loop(mlx);
 }
