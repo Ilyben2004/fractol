cad617329dea8946f8dc952bd1976e35717ce795
Ilyas Bennajeh <ibennaje@c4r1p8.1337.ma>
2025-02-17 05:59:24 +0100

    n9ina chwiya 3la l7ala

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: ibennaje <ibennaje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 23:07:00 by ibennaje          #+#    #+#             */
-/*   Updated: 2025/02/17 04:54:41 by ibennaje         ###   ########.fr       */
+/*   Updated: 2025/02/17 05:58:32 by ibennaje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,21 +51,21 @@ typedef struct s_cords
 
 } t_cords;
 
-typedef struct  s_compelxes
+typedef struct s_compelxes
 {
-	int	x;
-	int	y;
-	int	n;
-	int z_re;
-	int z_im;
-	int c_re;
-	int c_im;
-	int z_re2;
-	int z_im2;
+	int x;
+	int y;
+	int n;
+	double z_re;
+	double z_im;
+	double c_re;
+	double c_im;
+	double z_re2;
+	double z_im2;
+	int isinside;
 
 	/* data */
-} t_compelxes ;
-
+} t_compelxes;
 
 typedef struct s_vars
 {
@@ -75,15 +75,13 @@ typedef struct s_vars
 	int ini;
 } t_vars;
 
-void	img_pix_put(t_img *img, int x, int y, int color)
+void img_pix_put(t_img *img, int x, int y, int color)
 {
-    char    *pixel;
+	char *pixel;
 
-    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
-    *(int *)pixel = color;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
 }
-
-
 
 int get_color(int iterations)
 {
@@ -96,116 +94,103 @@ int get_color(int iterations)
 	return (iterations * offset);
 }
 
-// generated function just to see it more pretty
-// int get_color(int iterations)
-// {
-//     if (iterations == 0)
-//         return 0x000000;  // Black
 
-//     // Create a smooth color gradient
-//     int r = (iterations * 7) % 255;
-//     int g = (iterations * 5) % 255;
-//     int b = (iterations * 11) % 255;
-
-//     return (r << 16) | (g << 8) | b;
-// }
+void mandelbort_helper(t_vars *vars, t_compelxes *compelxes)
+{
+	while (compelxes->n < vars->cords.MaxIterations)
+	{
+		compelxes->z_re2 = compelxes->z_re * compelxes->z_re;
+		compelxes->z_im2 = compelxes->z_im * compelxes->z_im;
+		if (compelxes->z_re2 + compelxes->z_im2 > 4)
+		{
+			compelxes->isinside = 0;
+			break;
+		}
+		compelxes->z_im = 2 * compelxes->z_re * compelxes->z_im + compelxes->c_im;
+		compelxes->z_re = compelxes->z_re2 - compelxes->z_im2 + compelxes->c_re;
+		vars->cords.color = compelxes->n;
+		compelxes->n++;
+	}
+}
 
 void mandelbort_set(t_vars *vars)
 {
-	int x, y, n;
-	y = 0;
+	t_compelxes compelxes;
+	compelxes.y = -1;
 
+	while (++compelxes.y < vars->cords.ImageHeight)
+	{
+		compelxes.c_im = vars->cords.MaxIm - compelxes.y * vars->cords.Im_factor;
+		compelxes.x = -1;
+		while (++compelxes.x < vars->cords.ImageWidth)
+		{
+			compelxes.c_re = vars->cords.MinRe + compelxes.x * vars->cords.Re_factor;
+			compelxes.z_re = compelxes.c_re;
+			compelxes.z_im = compelxes.c_im;
+			compelxes.isinside = 1;
+			compelxes.n = 0;
+			mandelbort_helper(vars, &compelxes);
+			if (!compelxes.isinside)
+				img_pix_put(&(vars->fractal.img), compelxes.x, compelxes.y, get_color(vars->cords.color));
+		}
+	}
+}
+
+void julia_set(t_vars *vars)
+{
+	int x, y, n;
+	// Julia set constants - these give a nice default shape
+	double c_re = -0.7;
+	double c_im = 0.27015;
+
+	y = 0;
 	while (y < vars->cords.ImageHeight)
 	{
-		double c_im = vars->cords.MaxIm - y * vars->cords.Im_factor;
 		x = 0;
 		while (x < vars->cords.ImageWidth)
 		{
-			double c_re = vars->cords.MinRe + x * vars->cords.Re_factor;
+			// Calculate initial complex number using the same scaling as Mandelbrot
+			double Z_re = vars->cords.MinRe + x * vars->cords.Re_factor;
+			double Z_im = vars->cords.MaxIm - y * vars->cords.Im_factor;
 
-			double Z_re = c_re, Z_im = c_im;
 			int isInside = 1;
 			n = 0;
 			while (n < vars->cords.MaxIterations)
 			{
-				double Z_re2 = Z_re * Z_re, Z_im2 = Z_im * Z_im;
+				double Z_re2 = Z_re * Z_re;
+				double Z_im2 = Z_im * Z_im;
+
+				// Check if point escapes
 				if (Z_re2 + Z_im2 > 4)
 				{
 					isInside = 0;
 					break;
 				}
-				Z_im = 2 * Z_re * Z_im + c_im;
-				Z_re = Z_re2 - Z_im2 + c_re;
+
+				// Calculate next iteration
+				double Z_re_temp = Z_re;
+				Z_re = Z_re2 - Z_im2 + c_re; // Real part: z^2 + c
+				Z_im = 2 * Z_re_temp * Z_im + c_im;
+				// Imaginary part : z ^ 2 + c /
+
+				// Store iteration count for coloring
 				vars->cords.color = n;
 				n++;
 			}
-			if (!isInside)
-			{
 
+			// Color the pixel based on whether it's inside the set and iteration count
+			if (isInside)
+			{
+				img_pix_put(&(vars->fractal.img), x, y, 0x000000);
+			}
+			else
+			{
 				img_pix_put(&(vars->fractal.img), x, y, get_color(vars->cords.color));
 			}
 			x++;
 		}
 		y++;
 	}
-}
-
-void julia_set(t_vars *vars)
-{
-    int x, y, n;
-    // Julia set constants - these give a nice default shape
-    double c_re = -0.7; 
-    double c_im = 0.27015;
-
-    y = 0;
-    while (y < vars->cords.ImageHeight)
-    {
-        x = 0;
-        while (x < vars->cords.ImageWidth)
-        {
-            // Calculate initial complex number using the same scaling as Mandelbrot
-            double Z_re = vars->cords.MinRe + x * vars->cords.Re_factor;
-            double Z_im = vars->cords.MaxIm - y * vars->cords.Im_factor;
-            
-            int isInside = 1;
-            n = 0;
-
-            // Iterate using the Julia set formula: z = z^2 + c
-            while (n < vars->cords.MaxIterations)
-            {
-                double Z_re2 = Z_re * Z_re;
-                double Z_im2 = Z_im * Z_im;
-                
-                // Check if point escapes
-                if (Z_re2 + Z_im2 > 4)
-                {
-                    isInside = 0;
-                    break;
-                }
-                
-                // Calculate next iteration
-                double Z_re_temp = Z_re;
-                Z_re = Z_re2 - Z_im2 + c_re;      // Real part: z^2 + c
-                Z_im = 2 * Z_re_temp * Z_im + c_im; // Imaginary part: z^2 + c
-                
-                // Store iteration count for coloring
-                vars->cords.color = n;
-                n++;
-            }
-
-            // Color the pixel based on whether it's inside the set and iteration count
-            if (isInside)
-            {
-                img_pix_put(&(vars->fractal.img), x, y, 0x000000);
-            }
-            else
-            {
-                img_pix_put(&(vars->fractal.img), x, y, get_color(vars->cords.color));
-            }
-            x++;
-        }
-        y++;
-    }
 }
 
 void cords_init(t_cords *cords, int zoomchecker)
@@ -228,33 +213,91 @@ void cords_init(t_cords *cords, int zoomchecker)
 	cords->MaxIterations = 100;
 }
 
-int zoom_in(int keycode, int x, int y, t_vars *vars)
+void range_zoom_calculator(int keycode, double *range_re, double *range_im, t_vars *vars)
+{
+	double zoom = 1.1;
+	if (keycode == 4)
+	{
+		*range_re = (vars->cords.MaxRe - vars->cords.MinRe) / zoom;
+		*range_im = (vars->cords.MaxIm - vars->cords.MinIm) / zoom;
+	}
+	else
+	{
+		*range_re = (vars->cords.MaxRe - vars->cords.MinRe) * zoom;
+		*range_im = (vars->cords.MaxIm - vars->cords.MinIm) * zoom;
+	}
+	vars->cords.MinRe = vars->cords.cx - ((*range_re) / 2);
+	vars->cords.MaxRe = vars->cords.cx + ((*range_re) / 2);
+	vars->cords.MinIm = vars->cords.cy - ((*range_im) / 2);
+	vars->cords.MaxIm = vars->cords.cy + ((*range_im) / 2);
+	vars->cords.Re_factor = ((vars->cords.MaxRe - vars->cords.MinRe) / (vars->cords.ImageWidth - 1));
+	vars->cords.Im_factor = ((vars->cords.MaxIm - vars->cords.MinIm) / (vars->cords.ImageHeight - 1));
+}
+
+int zoom(int keycode, int x, int y, t_vars *vars)
 {
 	double range_re;
 	double range_im;
-	double zoom = 1.1;
 	void *old_image;
 
-	printf("(%d  , %d) \n", x, y);
 	if (keycode == 4 || keycode == 5)
 	{
-		if (keycode == 4)
-		{
-			range_re = (vars->cords.MaxRe - vars->cords.MinRe) / zoom;
-			range_im = (vars->cords.MaxIm - vars->cords.MinIm) / zoom;
-			printf("%f is the distance \n", vars->cords.MaxRe - vars->cords.MinRe);
-		}
-		else
-		{
-			range_re = (vars->cords.MaxRe - vars->cords.MinRe) * zoom;
-			range_im = (vars->cords.MaxIm - vars->cords.MinIm) * zoom;
-		}
-		vars->cords.MinRe = vars->cords.cx - ((range_re) / 2);
-		vars->cords.MaxRe = vars->cords.cx + ((range_re) / 2);
-		vars->cords.MinIm = vars->cords.cy - ((range_im) / 2);
-		vars->cords.MaxIm = vars->cords.cy + ((range_im) / 2);
-		vars->cords.Re_factor = ((vars->cords.MaxRe - vars->cords.MinRe) / (vars->cords.ImageWidth - 1));
-		vars->cords.Im_factor = ((vars->cords.MaxIm - vars->cords.MinIm) / (vars->cords.ImageHeight - 1));
+		range_zoom_calculator(keycode, &range_re, &range_im, vars);
+		old_image = vars->fractal.img.mlx_img;
+		vars->fractal.img.mlx_img = mlx_new_image(vars->fractal.mlx, vars->cords.ImageWidth, vars->cords.ImageHeight);
+		vars->fractal.img.addr = mlx_get_data_addr(vars->fractal.img.mlx_img,
+												   &vars->fractal.img.bpp, &vars->fractal.img.line_len,
+												   &vars->fractal.img.endian);
+		if (old_image)
+			mlx_destroy_image(vars->fractal.mlx, old_image);
+		vars->draw(vars);
+		mlx_put_image_to_window(vars->fractal.mlx, vars->fractal.mlx_win,
+								vars->fractal.img.mlx_img, 0, 0);
+	}
+}
+
+int destroy_window(t_vars *vars)
+{
+	mlx_destroy_image(vars->fractal.mlx, vars->fractal.img.mlx_img);
+	mlx_destroy_window(vars->fractal.mlx, vars->fractal.mlx_win);
+}
+int key_hook_helper(t_vars *vars, int keycode)
+{
+	if (keycode == 65363)
+	{
+		vars->cords.MinRe += (vars->cords.MaxRe - vars->cords.MinRe) / 20;
+		vars->cords.MaxRe += (vars->cords.MaxRe - vars->cords.MinRe) / 20;
+		return (1);
+	}
+	else if (keycode == 65361)
+	{
+		vars->cords.MinRe -= (vars->cords.MaxRe - vars->cords.MinRe) / 20;
+		vars->cords.MaxRe -= (vars->cords.MaxRe - vars->cords.MinRe) / 20;
+		return (1);
+	}
+	else if (keycode == 65362)
+	{
+		vars->cords.MinIm += (vars->cords.MaxIm - vars->cords.MinIm) / 20;
+		vars->cords.MaxIm += (vars->cords.MaxIm - vars->cords.MinIm) / 20;
+		return (1);
+	}
+	else if (keycode == 65364)
+	{
+		vars->cords.MinIm -= (vars->cords.MaxIm - vars->cords.MinIm) / 20;
+		vars->cords.MaxIm -= (vars->cords.MaxIm - vars->cords.MinIm) / 20;
+		return (1);
+	}
+	return (0);
+}
+
+int key_hook(int keycode, t_vars *vars)
+{
+	void *old_image;
+
+	if (key_hook_helper(vars , keycode))
+	{
+		vars->cords.cx = (vars->cords.MaxRe + vars->cords.MinRe) / 2;
+		vars->cords.cy = (vars->cords.MaxIm + vars->cords.MinIm) / 2;
 
 		old_image = vars->fractal.img.mlx_img;
 		vars->fractal.img.mlx_img = mlx_new_image(vars->fractal.mlx, vars->cords.ImageWidth, vars->cords.ImageHeight);
@@ -267,57 +310,11 @@ int zoom_in(int keycode, int x, int y, t_vars *vars)
 		mlx_put_image_to_window(vars->fractal.mlx, vars->fractal.mlx_win,
 								vars->fractal.img.mlx_img, 0, 0);
 	}
-	return (1);
-}
-
-int destroy_window(t_vars *vars)
-{
-	mlx_destroy_image(vars->fractal.mlx, vars->fractal.img.mlx_img);
-	mlx_destroy_window(vars->fractal.mlx, vars->fractal.mlx_win);
-}
-
-int shift_view(int keycode, t_vars *vars)
-{
-	void *old_image;
-	printf("key code %d \n", keycode);
-	if (keycode == 65363)
-	{
-		vars->cords.MinRe += (vars->cords.MaxRe - vars->cords.MinRe) / 20;
-		vars->cords.MaxRe += (vars->cords.MaxRe - vars->cords.MinRe) / 20;
-	}
-	else if (keycode == 65361)
-	{
-		vars->cords.MinRe -= (vars->cords.MaxRe - vars->cords.MinRe) / 20;
-		vars->cords.MaxRe -= (vars->cords.MaxRe - vars->cords.MinRe) / 20;
-	}
-	else if (keycode == 65362)
-	{
-		vars->cords.MinIm += (vars->cords.MaxIm - vars->cords.MinIm) / 20;
-		vars->cords.MaxIm += (vars->cords.MaxIm - vars->cords.MinIm) / 20;
-	}
-	else if (keycode == 65364)
-	{
-		vars->cords.MinIm -= (vars->cords.MaxIm - vars->cords.MinIm) / 20;
-		vars->cords.MaxIm -= (vars->cords.MaxIm - vars->cords.MinIm) / 20;
-	}
 	else if (keycode == 65307)
 	{
 		destroy_window(vars);
 		exit(0);
 	}
-	vars->cords.cx = (vars->cords.MaxRe + vars->cords.MinRe) / 2;
-	vars->cords.cy = (vars->cords.MaxIm + vars->cords.MinIm) / 2;
-
-	old_image = vars->fractal.img.mlx_img;
-	vars->fractal.img.mlx_img = mlx_new_image(vars->fractal.mlx, vars->cords.ImageWidth, vars->cords.ImageHeight);
-	vars->fractal.img.addr = mlx_get_data_addr(vars->fractal.img.mlx_img,
-											   &vars->fractal.img.bpp, &vars->fractal.img.line_len,
-											   &vars->fractal.img.endian);
-	if (old_image)
-		mlx_destroy_image(vars->fractal.mlx, old_image);
-	vars->draw(vars);
-	mlx_put_image_to_window(vars->fractal.mlx, vars->fractal.mlx_win,
-							vars->fractal.img.mlx_img, 0, 0);
 	return (1);
 }
 
@@ -337,8 +334,8 @@ int main(int ac, char **av)
 	vars.draw(&vars);
 	vars.ini = 1;
 	mlx_put_image_to_window(vars.fractal.mlx, vars.fractal.mlx_win, vars.fractal.img.mlx_img, 0, 0);
-	mlx_mouse_hook(vars.fractal.mlx_win, &zoom_in, &vars);
+	mlx_mouse_hook(vars.fractal.mlx_win, &zoom, &vars);
 	mlx_hook(vars.fractal.mlx_win, 17, 0, destroy_window, &vars);
-	mlx_key_hook(vars.fractal.mlx_win, shift_view, &vars);
+	mlx_key_hook(vars.fractal.mlx_win, key_hook, &vars);
 	mlx_loop(vars.fractal.mlx);
 }
